68e3792cb59f1a48643b855bf7bb17e9de3641cd
nombre-de-usuarioLujiCodegit config --global user.name nombre-de-usuarioLujiCode <malujanmansilla@gmail.com>
2023-05-26 21:05:28 -0300

    Actualizacion final de archivo README.md

--- sketch.js
+++ sketch.js
@@ -1,25 +1,9 @@
-//Basada en la obra de El Lissitzky."Golpead a los blancos con la cuña roja (Клином красным бей белых!)" Rusia, 1920.
-
-//-------Variables globales
-
-var radioMayor = 60;
-var x = radioMayor;
-var velocidad = 5;
-var direccion = 1;
-var anguloRotacion = 270;
-
-let anchoPincel = 5;
-
 function setup() {
   createCanvas(500, 500);
   background(255);
-  //frameRate (10);
 }
 
 function draw() {
-  //console.log("x: " + mouseX + "Y:" + mouseY);
-  //console.log(frameCount);
-
   //puntos
   strokeWeight(10);
   point(50, 50);
@@ -52,38 +36,6 @@ function draw() {
   strokeWeight(3);
   line(595, 0, 290, 395);
 
-  //triangulo CAPA 1
-  push();
-
-  x += velocidad; // Aumenta el valor de x
-
-  translate(x, height / 1);
-  rotate(radians(anguloRotacion));
-
-  //translate(250, 250);
-  //scale(2.0);
-  //rotate(radians(10));
-
   fill("#C62828");
   triangle(264, 329, 128, 70, 64, 122);
-
-  pop();
-
-  push();
-  //--------MOUSE PRESIONADO
-  if (mouseIsPressed) {
-    stroke(255, 255, 0);
-    strokeWeight(anchoPincel);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
-  pop();
-
-  //Pincel
-  /*noStroke();
-  fill (255 ,255 ,0);
-  ellipse (mouseX, mouseY, 20, 20);*/
 }
-
-document.oncontextmenu = function () {
-  return false;
-};
